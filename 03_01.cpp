f5b31b5a393f9fd820ebe6fef17148444d2a3888
Ivan Khripunov <khripunov.iv@phystech.edu>
2021-09-20 16:36:29 +0300

    version 1.2 // variable size to be fixed

--- 03_01.cpp
+++ 03_01.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+
+int main()
+{
+	int n;
+	cin >> n;
+	int const size = n;
+	int array[size];
+	
+	for (int i = 0; i < size; ++i) 
+		cin >> array[i];
+		
+	for (int i = size - 1; i >= 0; --i)
+		cout << array[i] << ' ';
+		
+}
